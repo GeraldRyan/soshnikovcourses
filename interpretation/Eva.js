4e085cfa03d22081cea39a2c8b132a96fd71ed88
GeraldRyan <gerald.ryan40@protonmail.com>
2022-09-19 22:35:00 -0500

    math finished

--- interpretation/Eva.js
+++ interpretation/Eva.js
@@ -5,14 +5,28 @@ const assert = require('assert');
  */
 class Eva {
     eval(exp){
+        // -----------------------------
+        // Self Evaluating Expressions
         if (isNumber(exp)){
             return exp;
         }
         if (isString(exp)){
             return exp.slice(1, -1)
         }
+
+        // ------------------------------
+        // Math operations
         if (exp[0] === '+'){
             return this.eval(exp[1]) + this.eval(exp[2])
+        }
+        if (exp[0] === '*'){
+            return this.eval(exp[1]) * this.eval(exp[2])
+        }
+        if (exp[0] === '/'){
+            return this.eval(exp[1]) / this.eval(exp[2])
+        }
+        if (exp[0] === '-'){
+            return this.eval(exp[1]) - this.eval(exp[2])
         }
         throw 'Unimplemented'
     }
@@ -31,5 +45,8 @@ assert.strictEqual(eva.eval(1), 1);
 assert.strictEqual(eva.eval('"foo"'), 'foo');
 assert.strictEqual(eva.eval(['+', 1, 5]), 6);
 assert.strictEqual(eva.eval(['+', ['+', 3, 2], 5]), 10)
+assert.strictEqual(eva.eval(['+', ['*', 3, 2], 5]), 11)
+assert.strictEqual(eva.eval(['+', ['-', 3, 2], 5]), 6)
+assert.strictEqual(eva.eval(['+', ['/', 3, 2], 5]), 6.5)
 
 console.log("All assertions passed")
