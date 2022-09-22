ef00a6c118dfb867005946cf5a720eb2d03035c9
GeraldRyan <gerald.ryan40@protonmail.com>
2022-09-21 23:03:16 -0500

    refactor test

---
+++ interpretation/__tests/test_runner.js
@@ -0,0 +1,23 @@
+const Environment = require("../Environment");
+const Eva = require("../Eva");
+
+const tests = [
+    require('./block.spec'),
+    require('./math.spec'),
+    require('./self_evaluating_expression.spec'),
+    require('./variables.spec')
+]
+
+const eva = new Eva(new Environment({
+    null: null,
+    true: true,
+    false: false,
+
+    VERSION: '0.1'
+}));
+
+tests.forEach(t =>{
+    t(eva)
+})
+
+console.log("All assertions passed")
