790d1ea785ff947e3aac44eee1785452f3b45c6e
GeraldRyan <gerald.ryan40@protonmail.com>
2022-10-28 22:08:24 -0500

    all tests pass for math/binaryexpression

--- parser_from_scratch/letter-rdp/__tests__/math.spec.js
+++ parser_from_scratch/letter-rdp/__tests__/math.spec.js
@@ -2,7 +2,7 @@ module.exports = test => {
     // Addition:
     test(`2+2;`, {
         type: 'Program',
-        body: {
+        body: [{
             type: 'ExpressionStatement',
             expression: {
                 type: 'BinaryExpression',
@@ -16,34 +16,34 @@ module.exports = test => {
                     value: 2
                 }
             }
-        }
+        }]
     })
 
     // Nested Binary Expressions:
-    // test(`3 + 2 - 2;`, {
-    //     type: 'Program',
-    //     body: {
-    //         type: 'ExpressionStatement',
-    //         expression: {
-    //             type: 'BinaryExpression',
-    //             operator: '-',
-    //             left: {
-    //                 type: 'BinaryExpression',
-    //                 operator: '+',
-    //                 left: {
-    //                     type: 'NumericLiteral',
-    //                     value: 3
-    //                 },
-    //                 right: {
-    //                     type: 'NumericLiteral',
-    //                     value: 2
-    //                 }
-    //             },
-    //             right: {
-    //                 type: 'NumericLiteral',
-    //                 value: 2
-    //             }
-    //         }
-    //     }
-    // })
+    test(`3 + 2 - 2;`, {
+        type: 'Program',
+        body: [{
+            type: 'ExpressionStatement',
+            expression: {
+                type: 'BinaryExpression',
+                operator: '-',
+                left: {
+                    type: 'BinaryExpression',
+                    operator: '+',
+                    left: {
+                        type: 'NumericLiteral',
+                        value: 3
+                    },
+                    right: {
+                        type: 'NumericLiteral',
+                        value: 2
+                    }
+                },
+                right: {
+                    type: 'NumericLiteral',
+                    value: 2
+                }
+            }
+        }]
+    })
 }
