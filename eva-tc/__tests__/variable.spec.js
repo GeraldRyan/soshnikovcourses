b2324733aad3bf63c7e25646890a76efb026fd77
GeraldRyan <gerald.ryan40@protonmail.com>
2022-11-22 11:22:20 -0600

    enhanced test, testing recursiveness

--- eva-tc/__tests__/variable.spec.js
+++ eva-tc/__tests__/variable.spec.js
@@ -7,6 +7,10 @@ module.exports = eva => {
     // variable declaration:
     test(eva, ['var', 'x', 10], Type.number );
 
+    // variable declaration with type check:
+    test(eva, ['var', ['y', 'number'], 10], Type.number );
+    test(eva, ['var', ['z', 'number'], 'x'], Type.number ); // can refer to variables as is called recursively
+
     // variable access:
     test(eva, 'x', Type.number );
 
